e951a6264b2fba8919ccb1a3187d404e6c184a12
VladimirMikhailov <netwix@gmail.com>
2017-12-02 15:17:02 +0300

    init

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,23 @@
+import "src/application"
+import React from "react"
+import ReactDOM from "react-dom"
+import PropTypes from "prop-types"
+
+const Hello = props => (
+  <div>Hello {props.name}!</div>
+)
+
+Hello.defaultProps = {
+  name: 'Panda'
+}
+
+Hello.propTypes = {
+  name: PropTypes.string
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Hello name="React" />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
